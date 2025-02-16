d9f44b0ea44a8bd80cc979ceb3cfd95b06639d7d
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-02-16 19:49:34 +0100

    feat: add PageWrapper

---
+++ components/PageWrapper.styled.ts
@@ -0,0 +1,13 @@
+"use client";
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  max-width: 100%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(13, 17, 23, 0.5);
+`;
