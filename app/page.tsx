3905b422a25e0b89fee30882ea8b58cbb0e1d056
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-02-16 23:32:41 +0100

    chore: cleanup main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,7 @@
-import { gql } from "@apollo/client";
 import { PageWrapper } from "~/components/PageWrapper/PageWrapper.styled";
 import { SearchBar } from "~/components/SearchBar/SearchBar";
-import createApolloClient from "~/lib/apolloClient";
-
-const queryExample = gql`
-  query {
-    search(type: REPOSITORY, query: "name:*react*", last: 100) {
-      repos: edges {
-        repo: node {
-          ... on Repository {
-            url
-
-            allIssues: issues {
-              totalCount
-            }
-            openIssues: issues(states: OPEN) {
-              totalCount
-            }
-          }
-        }
-      }
-    }
-  }
-`;
 
 export default async function Page() {
-  // const apolloClient = createApolloClient();
-  // const { data } = await apolloClient.query({ query: queryExample });
-
   return (
     <PageWrapper>
       <SearchBar />
