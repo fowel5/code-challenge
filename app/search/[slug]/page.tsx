afeb044ffc45346c71fb7d1b3e8762731680a19f
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-02-17 00:07:58 +0100

    feat: prepare repo search page

--- app/search/[slug]/page.tsx
+++ app/search/[slug]/page.tsx
@@ -1,10 +1,23 @@
 import { PageWrapper } from "~/components/PageWrapper/PageWrapper.styled";
+import createApolloClient from "~/lib/apolloClient";
+import { createRepositorySearchQuery } from "~/lib/graphql.utils";
 
 export default async function Page({
   params,
+  searchParams,
 }: {
   params: Promise<{ slug: string }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
-  const slug = (await params).slug;
-  return <PageWrapper>{slug}</PageWrapper>;
+  const slugToSearch = (await params).slug;
+  const resolvedSearchParams = await searchParams;
+  const endCursor = resolvedSearchParams.endCursor || "";
+  const page = resolvedSearchParams.page || 1;
+
+  const searchQuery = createRepositorySearchQuery({ slugToSearch, endCursor });
+
+  const apolloClient = createApolloClient();
+  const { data } = await apolloClient.query({ query: searchQuery });
+
+  return <PageWrapper>{slugToSearch}</PageWrapper>;
 }
