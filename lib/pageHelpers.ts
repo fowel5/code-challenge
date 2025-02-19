1134d358938bf3be7f79ad52f19d5299eb2ef4c1
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-02-19 08:41:08 +0100

    feat: add date helper

--- lib/pageHelpers.ts
+++ lib/pageHelpers.ts
@@ -3,3 +3,9 @@ import { redirect } from "next/navigation";
 export function redirectToMainPage() {
   return redirect("/");
 }
+
+export function getFullDate(date: Date): string {
+  return `${date.getUTCDate()}-${
+    date.getUTCMonth() + 1
+  }-${date.getUTCFullYear()}`;
+}
