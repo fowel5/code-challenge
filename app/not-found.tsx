d4ac669988dbe03725abcec3f3e11cbff92d91bd
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-02-19 12:36:44 +0100

    feat: add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <>
+      <h1>Page not found with Error 404</h1>
+      <h3>
+        Please go to the <a href="/">Main Page</a>
+      </h3>
+    </>
+  );
+}
